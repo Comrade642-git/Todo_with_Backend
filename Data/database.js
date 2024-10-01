7fd40bac189048bb2ca9123ca50e849d6f3ff7f4
Comrade642-git <meet.adityachandel@gmail.com>
2024-10-01 12:29:09 +0530

    Advanced to Atlas

--- Data/database.js
+++ Data/database.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose"
 const mongodb = () => {
     mongoose.connect(process.env.MONGO_URL)
-        .then(() => {
-            console.log("CONNECTION OPEN!!!"); // Connection successful
+        .then((c) => {
+            console.log(`DataBase is connected to ${c.connection.host}`); 
         })
         .catch(err => {
-            console.error("OH NO ERROR!!!!", err); // Connection failed
+            console.error("OH NO ERROR!!!!", err); 
         });
 }
 export default mongodb;
